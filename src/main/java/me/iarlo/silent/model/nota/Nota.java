2be161bf21620d53196c5b8e650b337d41abbe82
Caio <118756888+caiomagn0@users.noreply.github.com>
2025-07-21 09:39:13 -0300

    Update Nota.java

--- src/main/java/me/iarlo/silent/model/nota/Nota.java
+++ src/main/java/me/iarlo/silent/model/nota/Nota.java
@@ -1,33 +1,56 @@
 package me.iarlo.silent.model.nota;
 
+import me.iarlo.silent.model.cliente.Cliente;
+import me.iarlo.silent.model.produto.Produto;
+
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Nota {
-    private final LocalDateTime data;
-    private List<ItemNota> items;
-    private BigDecimal subtotal;
+    private Cliente cliente;
+    private List<ItemNota> items = new ArrayList<>();
+    private BigDecimal subtotal = BigDecimal.ZERO;
 
-    public Nota() {
-        this.data = LocalDateTime.now();
+    public Nota(Cliente cliente) {
+        this.cliente = cliente;
     }
 
-    public LocalDateTime getData() {
-        return this.data;
+    public void adicionarItem(ItemNota item) {
+        this.items.add(item);
     }
 
-    public BigDecimal getSubtotal() {
-        return this.subtotal;
+    public void calcularSubtotal() {
+        this.subtotal = BigDecimal.ZERO;
+        for (ItemNota item : items) {
+            Produto produto = item.getProduto();
+            BigDecimal precoTotal = produto.getPrecoBase().multiply(BigDecimal.valueOf(item.getQuantidade()));
+            this.subtotal = this.subtotal.add(precoTotal);
+        }
     }
 
     public void exibirResumo(int index) {
-        System.out.printf("%n--- NOTA %d - %s ---%n", index + 1, this.data);
-        for (ItemNota item : this.items) {
-            BigDecimal preco = item.getProduto().getPrecoBase().multiply(BigDecimal.valueOf(item.getQuantidade()));
-            System.out.printf("  %-25s (qnt. %d) R$ %s%n", item.getProduto().getNome(), item.getQuantidade(), preco);
+        System.out.printf("\n--- NOTA FISCAL %d ---%n", index + 1);
+        System.out.printf("Cliente: %s (%s)%n", cliente.getNome(), cliente.getCadastroPessoa());
+        for (ItemNota item : items) {
+            System.out.printf("Produto: %s | Qtd: %d | Preço: R$ %.2f%n",
+                item.getProduto().getNome(),
+                item.getQuantidade(),
+                item.getProduto().getPrecoBase());
         }
-        System.out.printf("Subtotal: %25s", this.getSubtotal());
-        System.out.printf("%n----------------------------%n");
+        System.out.printf("Subtotal: R$ %.2f%n", this.subtotal);
+        System.out.println("-----------------------------\n");
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<ItemNota> getItems() {
+        return items;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
     }
 }
